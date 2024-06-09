8ba7425201d345eda30c8cd46f0208ccf3ded901
svetk0 <sv.solomatnikova@gmail.com>
2024-06-09 20:49:22 +0300

    clear unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,45 @@
-// import { useEffect, useState } from "react";
-// import { getAuth, onAuthStateChanged } from "firebase/auth";
-// import { useDispatch } from "react-redux";
-// import { setUser } from "./Store/slices/userSlice";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { setUser } from "./Store/slices/userSlice";
+import { useAuth } from "./hooks/useAuth";
+import { Routes, Route, useNavigate, Link } from "react-router-dom";
 
 import Error404 from "./Pages/Error404/Error404.jsx";
 import HomeStudentPage from "./Pages/StudentPage/HomeStudentPage/HomeStudentPage.jsx";
 import ScheduleStudentPage from "./Pages/StudentPage/ScheduleStudentPage/ScheduleStudentPage.jsx";
 import MenuNav from "./Components/MenuNav/MenuNav.jsx";
-import Login from "./Pages/AuthPage/Login.jsx";
-import Registr from "./Pages/AuthPage/Registr.jsx";
+
 import LoginPage from './Pages/AuthPage/LoginPage.jsx';
 import SignUpPage from './Pages/AuthPage/RegistrationPage.jsx';
 import HomePage from "./Pages/AuthPage/HomePage.jsx";
+
 function App() {
-  // const auth = getAuth();
-  // const navigate = useNavigate();
-  // const [statusUser, setStatusUser] = useState('loading')
-  // const dispatch = useDispatch();
+  const auth = getAuth();
+  const { isAuth, email } = useAuth();
+  const navigate = useNavigate();
+  const [statusUser, setStatusUser] = useState('loading')
+  const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //     onAuthStateChanged(auth, (currentUser) => {
-  //         if (!currentUser) {
-  //             navigate("/login");
-  //             setStatusUser(null)
-  //             return;
-  //         }
-  //         dispatch(
-  //             setUser({
-  //                 email: currentUser.email,
-  //                 uid: currentUser.uid,
-  //                 nameUser: currentUser.displayName,
-  //                 group: currentUser.group,
-  //             })
-  //         );
-  //         //navigate('/home')
-  //         setStatusUser('successfully')
-  //     });
-  // }, []);
+  useEffect(() => {
+      onAuthStateChanged(auth, (currentUser) => {
+          if (!currentUser) {
+              navigate("/login");
+              setStatusUser(null)
+              return;
+          }
+          dispatch(
+              setUser({
+                  email: currentUser.email,
+                 
+              })
+          );
+          navigate('/')
+          setStatusUser('successfully')
+      });
+  }, []);
 
-  // if (statusUser === 'loading') return <Spinner />
+  
 
   return (
     <div className="App-main-container">
@@ -47,7 +47,7 @@ function App() {
         <MenuNav />
       </nav>
       <div className="App-main-wrapper">
-        <header>Header, welcoming</header>
+        <header>Header, {isAuth ? 'Welcome '+ email : ( <Link to="/login">Войти</Link>) }  </header>
 
         <main>
           <Routes>
@@ -56,7 +56,7 @@ function App() {
             <Route path="/sign-up" element={<SignUpPage />} />
             <Route path="/home-student" element={<HomeStudentPage />} />
             <Route path="/schedule" element={<ScheduleStudentPage />} />
-            {/* <Route path="/schedule-student" element={<ScheduleStudentPage />} /> */}
+           
 
             <Route path="*" element={<Error404 />} />
           </Routes>
--- src/Pages/AuthPage/HomePage.jsx
+++ src/Pages/AuthPage/HomePage.jsx
@@ -1,28 +1,27 @@
-import {redirect, Link} from 'react-router-dom';
-import {useDispatch} from 'react-redux'
-import { useAuth } from '../../hooks/useAuth';
+import { redirect, Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useAuth } from "../../hooks/useAuth";
 import { removeUser } from "../../Store/slices/userSlice";
 
 const HomePage = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const { isAuth, email } = useAuth();
 
-    const {isAuth, email} = useAuth();
+  return isAuth ? (
+    <div>
+      <h1>Welcome !</h1>
+      <div></div>
 
-    return isAuth ? (
-        <div>
-            <h1>Welcome</h1>
-            <div></div>
+      <button onClick={() => dispatch(removeUser())}>
+        Log out from {email}
+      </button>
+    </div>
+  ) : (
+    <div>
+      <div>Ooops, SIGN IN failure</div>
+      <Link to="/login">Вернуться на главную</Link>
+    </div>
+  );
+};
 
-            <button
-                onClick={() => dispatch(removeUser())}
-            >Log out from {email}</button>
-        </div>
-    ) : (
-            
-        <Link to="/login">Вернуться на главную</Link> 
-            
-       
-    )
-}
-
-export default HomePage
+export default HomePage;
